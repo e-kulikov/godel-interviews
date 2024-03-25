4c418ee1f4e4cb14446f760016c591bfbb6b6bf3
Evgeniy Kulikov <3277076+e-kulikov@users.noreply.github.com>
2024-03-25 16:09:51 +0000

    Changes from codesandbox
    - eslint apply

--- front/react/src/Pagination.tsx
+++ front/react/src/Pagination.tsx
@@ -1,13 +1,13 @@
 interface PaginationProps {
-    pageNumber: number
-    onSetPage: (page: number) => void
+  pageNumber: number;
+  onSetPage: (page: number) => void;
 }
 
 export const Pagination = ({ pageNumber, onSetPage }: PaginationProps) => {
-    return (
-        <div className="pagination">
-            <button onClick={() => onSetPage(pageNumber - 1)}>&lt; Previous</button>
-            <button onClick={() => onSetPage(pageNumber + 1)}>Next &gt;</button>
-        </div>
-    );
+  return (
+    <div className="pagination">
+      <button onClick={() => onSetPage(pageNumber - 1)}>&lt; Previous</button>
+      <button onClick={() => onSetPage(pageNumber + 1)}>Next &gt;</button>
+    </div>
+  );
 };
--- front/react/src/customer-utils.ts
+++ front/react/src/customer-utils.ts
@@ -1,42 +1,50 @@
 interface Ability {
-    ability: {
-        name: string
-    }
+  ability: {
+    name: string;
+  };
 }
 const parseAbilities = ({ abilities }: { abilities: Ability[] }) =>
-    abilities.map(({ ability }) => ability.name);
+  abilities.map(({ ability }) => ability.name);
 
 interface Sprites {
-    front_default: string
+  front_default: string;
 }
-const parsePicture = ({ sprites }: { sprites: Sprites }) => sprites.front_default;
+const parsePicture = ({ sprites }: { sprites: Sprites }) =>
+  sprites.front_default;
 
 export interface Customer {
-    name: string
+  name: string;
 }
 
 export interface CustomerListResponse {
-    results: Customer[]
+  results: Customer[];
 }
-export const parseCustomersList =
-    ({ results }: CustomerListResponse): Customer[] => results;
+export const parseCustomersList = ({
+  results,
+}: CustomerListResponse): Customer[] => results;
 
 export interface CustomerData extends Customer {
-    id: number
-    height: number
-    weight: number
-    abilities: string[]
-    picture: string
+  id: number;
+  height: number;
+  weight: number;
+  abilities: string[];
+  picture: string;
 }
 export type CustomerResponse = CustomerData & {
-    abilities: Ability[]
-    sprites: Sprites
-}
-export const parseCustomerData = ({ name, id, height, weight, ...customer }: CustomerResponse): CustomerData => ({
-    id,
-    name,
-    height,
-    weight,
-    abilities: parseAbilities(customer),
-    picture: parsePicture(customer),
+  abilities: Ability[];
+  sprites: Sprites;
+};
+export const parseCustomerData = ({
+  name,
+  id,
+  height,
+  weight,
+  ...customer
+}: CustomerResponse): CustomerData => ({
+  id,
+  name,
+  height,
+  weight,
+  abilities: parseAbilities(customer),
+  picture: parsePicture(customer),
 });
