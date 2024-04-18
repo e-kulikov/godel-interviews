03f7ba4cd9098479f2bdd7b6cceb07de945dca64
e.kulikov <e.kulikov@godeltech.com>
2024-04-18 13:36:15 +0000

    Server: Express server
    - improve logging in error handler middleware

--- server/express/src/middlewares/error.middleware.ts
+++ server/express/src/middlewares/error.middleware.ts
@@ -3,10 +3,12 @@ import createHttpError from 'http-errors'
 import { StatusCodes } from 'http-status-codes'
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+    console.group('*** ERROR HANDLER MIDDLEWARE ***')
     console.error(error);
+    console.groupEnd()
     
     if (createHttpError.isHttpError(error)) {
-        return res.status(error.statusCode).send(error.message);
+        return res.status(error.statusCode).send(error.message)
     }
 
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: 'Looks like Team Rocket\'s blasting off again!'})
